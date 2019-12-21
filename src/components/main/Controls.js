2ef0991ad47b700f065678d57a588bde94b61764
James Sharpe <jameswrightsharpe@gmail.com>
2019-12-21 09:21:38 -0500

    removed most react, the project will run into issues with slowdown if I go the react route

--- src/components/main/Controls.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Controls = () => {
-  return <div>controls</div>;
-};
-
-export default Controls;
--- src/components/main/Instruments.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Instruments = () => {
-  return <div>instruments</div>;
-};
-
-export default Instruments;
